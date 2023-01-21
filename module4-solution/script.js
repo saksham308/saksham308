9baa9d83cf614d5b4f82ece2f8137b2c41f736bc
saksham308 <110740459+saksham308@users.noreply.github.com>
2023-01-22 02:24:55 +0530

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -31,7 +31,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-
+function names(){
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
@@ -59,4 +59,5 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   } else {
     // helloSpeaker.xxxx
   }
+}
 }
